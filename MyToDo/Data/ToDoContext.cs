693aa0e5bf85fda9d2af1fd760f1deee36f3d24e
Martin Davis <martin_davis3@yahoo.ca>
2020-05-21 21:04:18 -0600

    working ToDo app

---
+++ MyToDo/Data/ToDoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyToDo.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoItem> ToDoItem { get; set; }
+    }
+}
